bf0771a19d09a1b96ae015a2966e9091adc0cbc8
StevenXPS13 <steven.parrott@gmail.com>
2019-05-07 10:37:51 -0700

    changed address to https

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,7 +4,7 @@ import keys from "./keys"
 export default {
     
 getWeather: function (city) {
-    return axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&APPID=${keys.openWeather.APIKEY}`)
+    return axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&APPID=${keys.openWeather.APIKEY}`)
 }
 
 };
